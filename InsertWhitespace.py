a1e7cdcbf364b242d9020fd3b70c8d718a102715
Benjamin Gill <git@bgill.eu>
2016-04-03 22:32:26 +0100

    Handle multiple lines correctly

--- InsertWhitespace.py
+++ InsertWhitespace.py
@@ -1,20 +1,29 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
+
 
 class InsertWhitespaceCommand(sublime_plugin.TextCommand):
     def run(self, edit):
 
-        # Separate out regions to cope with multiple selections
+        # Separate out regions to cope with multiple selections.
         for region in self.view.sel():
 
-            # get full lines
-            lines = self.view.substr(self.view.line(region)).split("\n")
+            # Update selection to include whole lines.
+            region = self.view.line(region)
+
+            # Extract individial lines.
+            lines = self.view.substr(region).split("\n")
             new_lines = []
 
             for line in lines:
                 new_line = line
+
                 if len(line) < 79:
-                    # If a line is shorter than 79 characters, append spaces
+
+                    # If a line is shorter than 79 characters, append spaces.
                     new_line += " " * (79 - len(line))
+
                 new_lines.append(new_line)
 
+            # Replace old selection with new version with extra spaces.
             self.view.replace(edit, region, "\n".join(new_lines))
